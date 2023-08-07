6759e32810c7159c6b84a44572792b3ac0946174
MikeAtom <sonicgod98@gmail.com>
2023-08-07 03:23:59 +0300

    Fixed inputDevicesList

--- options.py
+++ options.py
@@ -34,6 +34,10 @@ def write_settings():
 def write_default_setting():
     global userResolution, userDisplayMode, userGraphicsQuality, userVSync, userController, userPrompt, userLanguage, inputDevicesList
 
+    inputDevicesList = ['Keyboard + Mouse'] + [i.split(" - ")[1] for i in
+                                               [f"{i} - {pygame.joystick.Joystick(i).get_name()}" for i in
+                                                range(pygame.joystick.get_count())]]
+
     userController = inputDevicesList[-1]
 
     get_prompt()
